cc78d0160279d8de3377411b45bb0d2fc46bfdf3
Sagar Kansara <kansarasagar@ymail.com>
2016-02-28 14:06:18 -0600

    Update CTCI1_8.java

--- CTCI1_8.java
+++ CTCI1_8.java
@@ -2,30 +2,56 @@ import java.lang.*;
 public class CTCI1_8 {
 	public void is_substring(String a, String b)
 	{
-		boolean result= true;
-		String combine = a + a;
-		if(a.length()==b.length() && a.length()>0)
-		result = isSubstring(combine,a);
-		if(result)
-			System.out.println("True");
+		String combine = a+a;
+		if(is_rotation(combine,b))
+		{
+			System.out.println("String is rotation of first string");
+		}
 		else
-			System.out.println("False");
+		{
+			System.out.println("String is not rotation of first string");
+		}
 		
 	}
-	 public static boolean isSubstring(String string1, String string2) {
-	       if(string2.length()==0)
-	    	   return false;
-	       else if(string1.toLowerCase().contains(string2.toLowerCase()));
-	       	return true;
-	       
-	    	   
-	    }
+	public boolean is_rotation(String c,String d)
+	{
+		boolean result=true;
+		int temp=0;
+		char e =d.charAt(0);
+		if(d.length()>c.length())
+		{
+			return result=false;
+		}
+		
+		for(int i=0;i<d.length();i++)
+		{
+			if(c.charAt(i)==e)
+				temp = i;
+				
+				
+		}
+		for(int i=0;i<d.length();i++)
+		{
+			
+			if(c.charAt(temp)==d.charAt(i))
+			{
+				temp++;
+				result=true;
+				
+			}
+			else
+			{
+				result=false;
+			}
+		}
+		return result;
+	}
 	
 
 public static void main(String args[])
 	{
-		String str1 = "abcdef";
-		String str2 = "dabc";
+		String str1 = "waterbottle";
+		String str2 = "erbottlewat";
 		CTCI1_8 c = new CTCI1_8();
 		c.is_substring(str1,str2);
 		
